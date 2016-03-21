07bf68aac8af9ba86232a0d3215111666894e00b
Jonathan <mccode>
2016-03-21 16:02:31 -0400

    Removed unnecessary printing

--- src/parser/Main.java
+++ src/parser/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) {
         BufferedReader inputFile = null;
 
-        System.out.println("Attempting to open " + args[0] + "...");
         try {
             inputFile = new BufferedReader(new FileReader(new File(args[0])));
         } catch (IOException e) {
@@ -16,9 +15,7 @@ public class Main {
             System.out.println("ERROR: File could not be opened!");
             System.exit(1);
         }
-        System.out.println("Opening the file was successful!");
 
-        System.out.println("Parsing...");
         try {
             Parser parser = new CMinusParser(inputFile);
             parser.parse();
