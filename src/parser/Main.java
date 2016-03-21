0c1d738e1e95bdb15f536e431ce644714333c01d
Jonathan <mccode>
2016-03-21 15:43:13 -0400

    misc unnecessary fixes

--- src/parser/Main.java
+++ src/parser/Main.java
@@ -7,12 +7,10 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         BufferedReader inputFile = null;
-        PrintWriter writer = null;
 
         System.out.println("Attempting to open " + args[0] + "...");
         try {
             inputFile = new BufferedReader(new FileReader(new File(args[0])));
-            writer = new PrintWriter("parseTree.ast", "UTF-8");
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("ERROR: File could not be opened!");
--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -3,7 +3,6 @@ package parser;
 import scanner.LexicalErrorException;
 
 import java.io.IOException;
-import java.io.Reader;
 
 public interface Parser {
     Program parse() throws ParserException, IOException, LexicalErrorException;
